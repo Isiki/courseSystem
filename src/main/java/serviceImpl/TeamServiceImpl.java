8f454e2c5ef5f1e43654e34a02a3462c5e8ada11
Ashleywsm <945496127@qq.com>
2016-07-08 11:23:07 +0800

    wsm update version

--- src/main/java/serviceImpl/TeamServiceImpl.java
+++ src/main/java/serviceImpl/TeamServiceImpl.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import service.TeamService;
 
+import java.util.List;
+
 
 /**
  * Created by 陌上花开 on 2016/7/5.
@@ -27,6 +29,21 @@ public class TeamServiceImpl implements TeamService {
             teamDao.createTeam(team);
             return  "success";
 
+    }
+
+    public Team searchTeamByName(String name){
+        Team team=teamDao.getTeamByName(name);
+        return team;
+    }
+
+    public Team searchTeamById(String id){
+        Team team=teamDao.getTeamById(id);
+        return team;
+    }
+
+    public List<Team> getTeamsInCourse(String course_id){
+        List<Team> team=teamDao.getTeamInCourse(course_id);
+        return team;
     }
 
 }
